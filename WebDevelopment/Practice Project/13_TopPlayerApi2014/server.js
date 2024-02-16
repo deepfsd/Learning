2034e47249905d851605471a620a7a172df5b4e0
Deepanshu <ansudeep695@gmail.com>
2024-02-16 22:20:38 +0530

    some css panding and backend panding

--- WebDevelopment/Practice Project/13_TopPlayerApi2014/server.js
+++ WebDevelopment/Practice Project/13_TopPlayerApi2014/server.js
@@ -1,20 +1,61 @@
-import  Express  from "express";
+import Express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = Express();
 const port = 4000;
 const api_url = 'http://localhost:3000';
+var __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.use(Express.static("public"));
 
 const playerData = [
-        {
-            potential: '94',
-            position: 'LW',
-            team: 'Real Madrid',
-            nationality: 'Portugal',
-            name: 'Ronaldo',
-
-        }
+    {
+        potential: '94',
+        position: 'LW',
+        team: 'Real Madrid',
+        nationality: 'Portugal',
+        name: 'Ronaldo',
+        nationalityImagePath: '/images/portugal.png',
+        nationAlt: 'portugal',
+        teamImagePath: '/images/rm.png',
+        teamAlt: 'realmadrid',
+        playerImagePath: '/images/cr7.png',
+        playerAlt: 'cr7'
+    },
+    {
+        potential: '94',
+        position: 'CF',
+        team: 'Barcelona',
+        nationality: 'Argentina',
+        name: 'Messi',
+        nationalityImagePath: '/images/argentina.png',
+        nationAlt: 'argentina',
+        teamImagePath: 'images/barca.png',
+        teamAlt: 'barcelona',
+        playerImagePath: '/images/messi.png',
+        playerAlt: 'messi',
+    },
+    {
+        potential: '84',
+        position: 'ST',
+        team: 'Barcelona',
+        nationality: 'Brazil',
+        name: 'Neymar',
+        nationalityImagePath: '/images/brazil.png',
+        nationAlt: 'brazil',
+        teamImagePath: '/images/barca.png',
+        teamAlt: 'barcelona',
+        playerImagePath: '/images/neymar.png',
+        playerAlt: 'neymar'
+    }
 ]
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
+app.get('/', (req,res)=>{
+    res.render('index.ejs', {playerDetails: playerData})
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
 })
