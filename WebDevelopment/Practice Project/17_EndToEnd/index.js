4bc696c948d0a1149ef4bc5a45afd1fa7f8f5cf7
Deepanshu <ansudeep695@gmail.com>
2024-04-06 21:28:09 +0530

    Some work done

--- WebDevelopment/Practice Project/17_EndToEnd/index.js
+++ WebDevelopment/Practice Project/17_EndToEnd/index.js
@@ -53,23 +53,67 @@ const validationcheck = [
 ]
 
 app.post('/submit',
-       validationcheck
+    validationcheck
     , (req, res) => {
 
         // console.log(name.trim() === '');
         const error = validationResult(req);
         if (!error.isEmpty()) {
-            res.render('signupPage.ejs', {err: error.mapped()});
+            res.render('signupPage.ejs', { err: error.mapped() });
         } else {
-            
+            const name = req.body.name;
+            const username = req.body.username;
+            const password = req.body.password;
+
+            try {
+                db.query('INSERT INTO userdata(name, username, password) VALUES ($1, $2, $3)', [name, username, password], (err, result) => {
+                    if (err) {
+                        res.render('signupPage.ejs', { userError: 'username already exist' });
+                    } else {
+                        res.render('loggedHomePage.ejs', { success: 'successfully registered' });
+                    }
+                });
+            } catch (error) {
+                res.render('signupPage.ejs', { userError: 'an error occurred while registering.' });
+            }
         }
     })
 
-app.post('/next', (req, res) => {
-    req.session.viewCount = req.body.next;
-    res.render('index2.ejs', { counts: req.session.viewCount });
-})
+app.post('/loginSubmit',
+    check('username').notEmpty().withMessage('username is required'),
+    check('password').notEmpty().withMessage('password is required')
+    , async (req, res) => {
+        const error = validationResult(req);
+        if (!error.isEmpty()) {
+            res.render('loginPage.ejs', { err: error.mapped() });
+        } else {
+            try {
+                const user = req.body.username;
+                const pass = req.body.password;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-});
+                const checkData = await db.query('SELECT password FROM userdata WHERE username = $1', [user], (err,result)=>{
+                    if(result.rowCount === 0){
+                        res.render('loginPage.ejs', {error: 'incorrect username'});
+                    }else{
+                        if(result.rows[0].password === pass){
+                            res.render('loggedHomePage.ejs');
+                        }else{
+                            res.render('loginPage.ejs', {error: 'incorrect password'});
+                        }
+                    }
+                });
+
+            } catch {
+
+            }
+        }
+    });
+
+        app.post('/next', (req, res) => {
+        req.session.viewCount = req.body.next;
+        res.render('index2.ejs', { counts: req.session.viewCount });
+    })
+
+        app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`)
+    });
